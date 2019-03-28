89fdcde49d51a5c92526ed30d7d5fe90fa28b2c6
Kevin Ruscoe <kevin@popcreative.co.uk>
2019-03-28 16:50:07 +0000

    add tests

--- src/PostcodeValidator.php
+++ src/PostcodeValidator.php
@@ -8,12 +8,13 @@ class PostcodeValidator
      * Validates a postcode.
      *
      * @param string $postcode
+     * @see https://stackoverflow.com/a/14935054
      * @return bool
      */
     public static function validate(string $postcode)
     {
-        $regex = '/[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}/m';
+        $regex = '#^(GIR ?0AA|[A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]([0-9ABEHMNPRV-Y])?)|[0-9][A-HJKPS-UW]) ?[0-9][ABD-HJLNP-UW-Z]{2})$#';
 
-        return preg_match($postcode, $regex);
+        return (bool) preg_match($regex, strtoupper($postcode));
     }
 }
---
+++ tests/PostcodeValidatorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KevinRuscoe\PostcodeToLatLng;
+
+use PHPUnit\Framework\TestCase;
+
+class PostcodeValidatorTest extends TestCase
+{
+    public function test_it_can_validate()
+    {
+        $this->assertTrue(PostcodeValidator::validate("L4 4EL"));
+        $this->assertTrue(PostcodeValidator::validate("l4 4el"));
+        $this->assertTrue(PostcodeValidator::validate("L44EL"));
+        $this->assertTrue(PostcodeValidator::validate("l44el"));
+        $this->assertTrue(PostcodeValidator::validate("l44El"));
+
+        $this->assertFalse(PostcodeValidator::validate("l44E l"));
+        $this->assertFalse(PostcodeValidator::validate("l 44El"));
+        $this->assertFalse(PostcodeValidator::validate(""));
+    }
+}
